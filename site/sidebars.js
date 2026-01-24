4d46da10dc03375ef32511740ce73a95328e1d5a
79pm-QC <79pm@outlook.com>
2026-01-24 10:10:49 +0000

    Refactor codexSidebar structure in sidebars.js

--- site/sidebars.js
+++ site/sidebars.js
@@ -1,33 +1,29 @@
 module.exports = {
-    codexSidebar: [
-        {
-              type: 'doc',
-                    id: 'intro',
-                          label: 'Qualian Codex',
-                              },
-                                  {
-                                        type: 'category',
-                                              label: 'Master Codex',
-                                                    items: [],
-                                                        },
-                                                            {
-                                                                  type: 'category',
-                                                                        label: 'Science Codex',
-                                                                              items: [],
-                                                                                  },
-                                                                                      {
-                                                                                            type: 'category',
-                                                                                                  label: 'Practice Codex',
-                                                                                                        items: [],
-                                                                                                            },
-                                                                                                                {
-                                                                                                                      type: 'category',
-                                                                                                                            label: 'Applications',
-                                                                                                                                  items: [],
-                                                                                                                                      },
-                                                                                                                                        ],
-                                                                                                                                        };
-}
-
-
-// Lock Codex sidebar structure
+  codexSidebar: [
+    {
+      type: 'doc',
+      id: 'intro',
+      label: 'Qualian Codex',
+    },
+    {
+      type: 'category',
+      label: 'Master Codex',
+      items: [],
+    },
+    {
+      type: 'category',
+      label: 'Science Codex',
+      items: [],
+    },
+    {
+      type: 'category',
+      label: 'Practice Codex',
+      items: [],
+    },
+    {
+      type: 'category',
+      label: 'Applications',
+      items: [],
+    },
+  ],
+};
