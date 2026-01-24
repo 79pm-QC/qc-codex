24e663b456621cbb6127df129428a3c15505ae35
79pm-QC <79pm@outlook.com>
2026-01-24 10:53:11 +0000

    Update sidebars.js

--- site/sidebars.js
+++ site/sidebars.js
@@ -1,33 +1,5 @@
 module.exports = {
   codexSidebar: [
-    {
-      type: 'doc',
-      id: 'intro',
-      label: 'Qualian Codex',
-    },
-
-    {
-      type: 'category',
-      label: 'Master Codex',
-      items: ['intro'],
-    },
-
-    {
-      type: 'category',
-      label: 'Science Codex',
-      items: ['intro'],
-    },
-
-    {
-      type: 'category',
-      label: 'Practice Codex',
-      items: ['intro'],
-    },
-
-    {
-      type: 'category',
-      label: 'Applications',
-      items: ['intro'],
-    },
+    { type: 'doc', id: 'intro', label: 'Qualian Codex' },
   ],
 };
