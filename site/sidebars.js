eeee2f82ef7c036ee9dc0acf104ebd454dfaead8
79pm-QC <79pm@outlook.com>
2026-01-30 13:47:34 +0000

    Update sidebars.js

--- site/sidebars.js
+++ site/sidebars.js
@@ -1,5 +1,13 @@
 module.exports = {
   codexSidebar: [
     { type: 'doc', id: 'intro', label: 'Qualian Codex' },
+
+    {
+      type: 'category',
+      label: 'Tools',
+      items: [
+        'tools/ccm-analyzer-lite',
+      ],
+    },
   ],
 };
